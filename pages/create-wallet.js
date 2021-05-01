0ebe0b892260efdf4ae0d1fdf73c2c56c2bfa0fa
shivaluma <shivaluma@gmail.com>
2021-05-01 11:15:19 +0700

    create ui, index and wallet

---
+++ pages/create-wallet.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+
+export default function CreateWallet() {
+
+
+  return <>
+    <div className="px-8 bg-white py-7">
+      <Header />
+    </div>
+  </>
+}
